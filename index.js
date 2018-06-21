9f725a47db542930218ef089710e9b76dd873061
Donnie Johnson <donnie_johnson82@yahoo.com>
2018-06-21 15:28:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Donnie"
-var height = "69"
+var height = 74
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(height + 1)
